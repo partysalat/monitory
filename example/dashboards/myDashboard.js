8aed32e728989c1dbc89dcad2f2c3f46559a5106
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-07-02 21:39:19 +0200

    Add grid to example project

--- example/dashboards/myDashboard.js
+++ example/dashboards/myDashboard.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { Flex, Box } from 'grid-styled';
 import Dashboard from '../../lib/frontend/Dashboard';
 import Card from '../../lib/frontend/widgets/Card';
 
 
 export default function () {
   return (
-    <Dashboard>
-      <Card job="example1" title="Waidmanns Heil! Die Luder festnageln die hanebüchen Das hochnäsig " threshold={200} value={({ current = {} }) => current.randomNumber} />
-      <Card job="example2" title="Example2" threshold={200} value={({ current = {} }) => current.randomNumber} />
-    </Dashboard>
+    <Flex>
+      <Box width={1 / 2} >
+        <Dashboard>
+          <Card job="example1" title="Waidmanns Heil! " threshold={200} value={({ current = {} }) => current.randomNumber} />
+          <Card job="example2" title="Example2" threshold={200} value={({ current = {} }) => current.randomNumber} />
+        </Dashboard>
+      </Box>
+      <Box width={1 / 2} >
+        <Dashboard>
+          <Card job="example1" title="Waidmanns Heil! Die Luder festnageln die hanebüchen Das hochnäsig " threshold={200} value={({ current = {} }) => current.randomNumber} />
+          <Card job="example2" title="Example2" threshold={200} value={({ current = {} }) => current.randomNumber} />
+        </Dashboard>
+      </Box>
+    </Flex>
   );
 }
