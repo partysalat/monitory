bee331917a625fc99c0a5a26f4c7e346b7da2198
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-07-13 21:50:53 +0200

    export higher order components for usage

--- frontend.js
+++ frontend.js
@@ -1,4 +1,5 @@
 import Card from './lib/frontend/widgets/Card';
 import Dashboard from './lib/frontend/Dashboard';
+import hoc from './lib/frontend/util';
 
-export { Card, Dashboard };
+export { Card, Dashboard, hoc };
