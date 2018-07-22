4834942b093834bf6e2b96f9aff4de0de9cb5235
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-07-22 20:08:58 +0200

    Make CountUpto Component accepting string values

--- lib/frontend/utils/CountUpto.jsx
+++ lib/frontend/utils/CountUpto.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CountUp from 'react-countup';
 import PropTypes from 'prop-types';
-
+import isNumber from 'lodash/isNumber';
 
 class CountUpto extends Component {
   componentWillMount() {
@@ -13,7 +13,9 @@ class CountUpto extends Component {
     const { value } = this.props;
     const { lastValue } = this;
     this.lastValue = value;
-
+    if (!isNumber(lastValue) || !isNumber(value)) {
+      return <span>value</span>;
+    }
     return (
       <CountUp start={lastValue} end={value} {...this.props} />
     );
