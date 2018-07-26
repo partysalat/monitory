3c0b44722ce5ff43f037a553d41f7d684057f2a8
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-07-26 20:51:32 +0200

    Change cron value in example

--- example/jobs/example1.job.js
+++ example/jobs/example1.job.js
@@ -8,7 +8,7 @@ function sharedJobFunction(number) {
 
 module.exports = [{
   id: 'example1',
-  cron: '*/2 * * * * *', // ms
+  cron: '0/2 * * * * *', // ms
   job: sharedJobFunction.bind(null, 20000),
 }, {
   id: 'example2',
