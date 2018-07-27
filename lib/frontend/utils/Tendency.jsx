6b1f1f0803a1af79e3fecfae0ee8c36501b17942
Ben Lenser <ben.lenser@scout24.com>
2018-07-27 08:24:50 +0200

    Implement Arrow for tendency

--- lib/frontend/utils/Tendency.jsx
+++ lib/frontend/utils/Tendency.jsx
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { ArrowRightCircle as Arrow } from 'styled-icons/feather/ArrowRightCircle';
 import { ThemeConsumer } from './Theme';
 
 const AbsoluteContainer = styled.div`
   flex: 0 0 auto;
-  font-size: 0.7rem;  
+  font-size: 0.7rem;
+  margin-top:5px;
 `;
 
-
+const StyledArrow = Arrow.extend`
+  height: 1.5rem;
+  transition-duration: 0.8s;
+  transition-property: transform;
+  transform : rotate(${({ rotation }) => css`${rotation}`});
+  color: ${({ color }) => css`${color}`}
+`;
 class Tendency extends Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = { rotation: 0, last: 0 };
   }
-
+  componentWillReceiveProps() {
+    this.setState(({ last }, { viewValue }) => ({
+      rotation: viewValue - last > 0 ? '-45deg' : '45deg',
+      last: viewValue,
+    }));
+  }
   render() {
-    const { viewValue, withTendency } = this.props;
+    const { withTendency } = this.props;
     if (!withTendency) {
       return null;
     }
@@ -23,11 +36,10 @@ class Tendency extends Component {
       <ThemeConsumer>
         {theme => (
           <AbsoluteContainer>
-            {viewValue}
+            <StyledArrow rotation={this.state.rotation} color={theme.fontColor} />
           </AbsoluteContainer>
       )}
-      </ThemeConsumer>
-    );
+      </ThemeConsumer>);
   }
 }
 
