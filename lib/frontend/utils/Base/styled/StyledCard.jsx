8016b9e6c3728d6be05c62b79abfc7c30ffe0ff5
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-07-31 20:42:18 +0200

    Simplify animation choosing in Styled Card

--- lib/frontend/utils/Base/styled/StyledCard.jsx
+++ lib/frontend/utils/Base/styled/StyledCard.jsx
@@ -1,13 +1,13 @@
 import styled, { css, keyframes } from 'styled-components';
-import { flipInX, pulse } from 'react-animations';
+import { flipInX as normalAnimation, pulse as alertAnimation} from 'react-animations';
 
 
-const bounceAnimation = keyframes`${flipInX}`;
-const pulseAnimation = keyframes`${pulse}`;
+const normalKeyframe = keyframes`${normalAnimation}`;
+const alertKeyframe = keyframes`${alertAnimation}`;
 
 
 export default styled.div`
-  animation: ${({ alert }) => (alert ? css`1s ${pulseAnimation} infinite` : css`1s ${bounceAnimation}`)};
+  animation: ${({ alert }) => (alert ? css`1s ${alertKeyframe} infinite` : css`1s ${normalKeyframe}`)};
   grid-column: span ${({ cols }) => css`${cols}`};
   grid-row: span ${({ rows }) => css`${rows}`};
   
