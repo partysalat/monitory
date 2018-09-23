d51c6e7f397ca215d08860258addad61f77c4517
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-09-23 18:33:48 +0200

    Add custom tile

---
+++ lib/frontend/widgets/CustomTile.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { isFunction } from 'lodash';
+
+import { withSubscription, withViewValue } from '../hoc';
+import Base from '../utils/Base';
+import Content from '../utils/Content';
+
+const Card = (props) => {
+  const {
+    children,
+    viewValue,
+    current,
+  } = props;
+  return (
+    <Base {...props} >
+      <Content>
+        {isFunction(children) ? children(current, viewValue) : children}
+      </Content>
+    </Base>);
+};
+
+
+export default compose(
+  withSubscription,
+  withViewValue,
+)(Card);
+
+Card.propTypes = {
+  job: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  showWhen: PropTypes.func,
+  color: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.func,
+  ]),
+  alert: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.bool,
+  ]),
+  cols: PropTypes.number,
+  rows: PropTypes.number,
+  value: PropTypes.func,
+
+};
