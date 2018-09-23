ed9cec7106ac50ab6a707a1f504a57f7e18d8020
Ben Lenser <ben.lenser@immobilienscout24.de>
2018-09-23 18:38:01 +0200

    Export hocs

--- frontend.js
+++ frontend.js
@@ -2,5 +2,16 @@ import { Card, List, ReloadableIframe, ReloadableImg } from './lib/frontend/widg
 import Dashboard from './lib/frontend/Dashboard';
 import { ThemeProvider, themes } from './lib/frontend/utils/Theme';
 import * as helpers from './lib/frontend/helpers';
+import * as hoc from './lib/frontend/hoc';
 
-export { List, Card, Dashboard, ReloadableIframe, ReloadableImg, ThemeProvider, themes, helpers };
+export {
+  List,
+  Card,
+  Dashboard,
+  ReloadableIframe,
+  ReloadableImg,
+  ThemeProvider,
+  themes,
+  helpers,
+  hoc,
+};
