f8ffe9f55571207d104e56165646541d7aeedc75
Ben Lenser <ben.lenser@scout24.com>
2019-04-05 14:01:41 +0200

    Remove styled normalize and use plain normalize.css

--- lib/frontend/main.template.jsx
+++ lib/frontend/main.template.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import styledNormalize from 'styled-normalize';
+import normalizeCss from 'normalize.css/normalize.css';
 import { injectGlobal, css } from 'styled-components';
 import toastCss from 'react-toastify/dist/ReactToastify.css';
 import chartistCss from 'chartist/dist/chartist.min.css';
@@ -11,7 +11,7 @@ import getStore from './redux';
 
 /* eslint-disable */
 injectGlobal`
-  ${styledNormalize}
+  ${css`${normalizeCss}`}
   ${toastCss.toString()}
   ${css`${chartistCss}`}
   body{
